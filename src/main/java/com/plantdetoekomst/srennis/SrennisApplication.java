64819119ea1f08cc586bf0204eb37f62be62aadf
eva burghoorn <r0878872@student.thomasmore.be>
2022-11-15 22:07:06 +0100

    backend last changes

--- src/main/java/com/plantdetoekomst/srennis/SrennisApplication.java
+++ src/main/java/com/plantdetoekomst/srennis/SrennisApplication.java
@@ -3,6 +3,7 @@ package com.plantdetoekomst.srennis;
 import com.sun.source.tree.TreeVisitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class SrennisApplication {
 
 	public static void main(String[] args) {
