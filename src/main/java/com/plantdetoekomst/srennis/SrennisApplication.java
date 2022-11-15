a0a1cd46aef6282d4cad04d0a23c43621779b713
eva burghoorn <r0878872@student.thomasmore.be>
2022-11-15 10:29:28 +0100

    add java spring

---
+++ src/main/java/com/plantdetoekomst/srennis/SrennisApplication.java
@@ -0,0 +1,13 @@
+package com.plantdetoekomst.srennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrennisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantdetoekomst/srennis/SrennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantdetoekomst.srennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
